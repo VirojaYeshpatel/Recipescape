e54f04a5e1cc8537e56e8130489d5965efbc53a1
VirojaYeshpatel <yeshpatel180599@gmail.com>
2025-08-25 16:10:12 +0530

    Updated index.js with DB changes

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,21 @@ app.use(cors({
 })); //cross orign request
 app.use(express.json()); //parses incoming request
 
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false, // Render requires SSL
+    },
 });
+
 
 
 
